59ec746680d2c2f6a35282be6e832b7ccd2b3534
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-02-09 01:00:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	 let count = 0;
+
+    // Keep dividing n by powers of 5 and update the count
+    while (n >= 5) {
+        n = Math.floor(n / 5);
+        count += n;
+    }
+
+    return count;
 }
 
 const input = prompt("Enter a number");
